a95f422ed62240787463a0fc6e5b4f48ebc56606
Rafinha <131063922+RafaBertacelli@users.noreply.github.com>
2024-03-18 23:40:37 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WAYSYS</title>
+    
+</head>
+<body>
+<div class="containr">
+        <nav class="cabecalho">
+                <div class="logo">
+                    <img class="logoBranca" src="images/logoBranca.png" >
+                </div>
+                <div class="links">
+                    
+                    <a class="caminhos" href="index.php"> QUEM SOMOS </a>
+                    <a class="caminhos" href="home.php"> HOME </a>
+                    <a  class="caminhos" href="projetos.php"> PROJETOS </a>
+                    <a  class="caminhos" href="faleConosco.php"> FALE CONOSCO </a>
+                </div>     
+        </nav>
+
